5908b3d44273a57244659bc32302cce127e34f0f
onedayonecommit <rudghks0981@gmail.com>
2022-12-22 14:33:32 +0900

    toolkit add

--- front/src/index.js
+++ front/src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
+import Store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
