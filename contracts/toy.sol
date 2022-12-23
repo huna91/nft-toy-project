5a7ed097ee494c9c3b7353d16140d7f8bd63b0ef
acca3434 <https://github.com/acca3434>
2022-12-23 17:52:27 +0900

    대량 민팅 완료

--- contracts/toy.sol
+++ contracts/toy.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
-import "../node_modules/openzeppelin-solidity/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 // import "./gazeaErc20.sol";
 
 contract NFTtoken is ERC721Enumerable,Ownable {
@@ -11,7 +11,7 @@ contract NFTtoken is ERC721Enumerable,Ownable {
     // 누가 어떤 NFT를 뽑았나 이벤트
     event nftTokenList(address nftTokenAddress,uint256 nftTokenNum);
     // NFT토큰의 최대량
-    uint256 public constant MAX_TOKEN_COUNT = 60;
+    uint256 public constant MAX_TOKEN_COUNT = 61;
     // mint할때마다 가격 기준
     uint256 public mint_price = 1 wei;
     // meta URI
@@ -24,15 +24,15 @@ contract NFTtoken is ERC721Enumerable,Ownable {
     ) ERC721(_name, _symbol) {
         metadataURI = _metadataURI;
         // NFT 배열에 대한 동적 할당
-        for(uint i = 0; i<MAX_TOKEN_COUNT; i++){
+        for(uint i = 1; i<MAX_TOKEN_COUNT; i++){
             tokenCount.push(i);
+            _mint(msg.sender, i);
         }
     }
     // 1~100까지 배열 동적 변수
     uint256[] public tokenCount; 
     // 민트하는 함수
     function mintToken() public payable {
-
         require(msg.value > mint_price,"Your minting value is less than our minting standard");
         require(MAX_TOKEN_COUNT > totalSupply(),"You've exceeded the value that you can mint per day");
         uint256 total = totalSupply() +1;
@@ -57,7 +57,22 @@ contract NFTtoken is ERC721Enumerable,Ownable {
     function setMetadataURI(string memory _uri) public {
         metadataURI = _uri;
     }
-
+    function mint(uint256 _tokenId) public {
+        _mint(msg.sender, _tokenId);
+    }
+    function mintArr(uint256 _tokenId) public {
+        for(uint i = 1; i<_tokenId; i++){
+            _mint(msg.sender, i);
+        }
+    }
+    function burn(uint256 _tokenId) public {
+        _burn(_tokenId);
+    }
+    function burnArr(uint256 _tokenId) public {
+        for(uint i = 1; i<_tokenId; i++){
+            _burn(i);
+        }
+    }
     function getTokenCount() public view returns (uint256[] memory) {
         return tokenCount;
     }
